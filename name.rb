ae2337b4c84260ee29dde3659dfc9a47ac565143
dulqad <dulqadhassan@gmail.com>
2022-09-23 07:15:54 +0300

    added two puts to ask the user to type there first and last name

--- name.rb
+++ name.rb
@@ -1,8 +1,13 @@
 #this program asks the user to type in their name and then prints out a greeting message with their name included.
 
+puts "what is your first name"
 first_name = gets.chomp
+puts "what is your last name"
 last_name = gets.chomp
 full_name = first_name + " " + last_name
 puts "Hello! #{full_name}"
-10.times {puts full_name}
+
+10.times do 
+    puts full_name
+end
 
